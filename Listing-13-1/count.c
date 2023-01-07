8a7f12ebc1bb50dbb222235a1fda9b7ff812be2b
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-07 07:17:15 -0600

    Updating Listing-13-1 directory

--- Listing-13-1/count.c
+++ Listing-13-1/count.c
@@ -5,25 +5,25 @@
 
 int main(int argc, char *argv[]) {
 
-	int ch;
-	FILE *fp;
-	long count = 0;
+	int ch; 	// place to store each character as read
+	FILE *fp;	// "file pointer"
+	unsigned long count = 0;
 
 	if (argc != 2) {
 
 		printf("Usage: %s filename\n", argv[0]);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	if ((fp = fopen(argv[1], "r")) == NULL) {
 
 		printf("Can't open %s\n", argv[1]);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	while ((ch = getc(fp)) != EOF) {
 		
-		putc(ch, stdout);
+		putc(ch, stdout); // same as putchar(ch);
 		count++;
 	}
 	fclose(fp);
