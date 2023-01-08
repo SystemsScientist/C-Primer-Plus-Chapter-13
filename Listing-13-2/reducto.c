61942f24f2e00701cb4250b44d4867848d4c53b1
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-08 06:05:35 -0600

    Updating Listing-13-2 directory files

--- Listing-13-2/reducto.c
+++ Listing-13-2/reducto.c
@@ -1,16 +1,16 @@
 // reducto.c -- reduces your files by two-thirds!
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> // for exit()
+#include <string.h> // for strcpy(), strcat()
 
 #define LEN 40
 
 int main(int argc, char *argv[]) {
 
-	FILE *in, *out;
+	FILE *in, *out; // declare two FILE pointers
 	int ch;
-	char name[LEN];
+	char name[LEN]; // storage for output filename
 	int count = 0;
 
 	// check for command-line arguments
@@ -29,10 +29,10 @@ int main(int argc, char *argv[]) {
 	}
 
 	// set up output
-	strncpy(name, argv[1], LEN - 5);
+	strncpy(name, argv[1], LEN - 5); // copy filename
 	name[LEN - 5] = '\0';
-	strcat(name, ".red");
-	if ((out = fopen(name, "w")) == NULL) {
+	strcat(name, ".red");		 // append .red
+	if ((out = fopen(name, "w")) == NULL) { // open file for writing
 
 		fprintf(stderr, "Can't create output file.\n");
 		exit(3);
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 	// copy data
 	while ((ch = getc(in)) != EOF)
 		if (count++ % 3 == 0)
-			putc(ch, out);
+			putc(ch, out); // print every 3rd char
 
 	// clean up
 	if (fclose(in) != 0 || fclose(out) != 0)
